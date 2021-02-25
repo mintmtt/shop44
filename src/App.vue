f79d504f4ad236d3d9d01ce2bdb6f774bc2ea67c
mintmtt <satidasomjai@gmail.com>
2021-02-25 21:47:53 +0700

    3

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
   <div id="app">
-    aaaaaaaaaaaaaa
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <div class="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-3"></div>
+          <div class="col-6" style="padding-bottom: 2rem;"><Cart /></div>
+          <div class="col-3"></div>
+           <div class="col-12"><Shop /></div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Shop,
+    Header,
+    Cart,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Times New Roman, Helvetica, Arial, sans-serif;
+  font-size: 15px;
+  background-color: rgb(202, 191, 212);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav class="navbar navbar-light" style="background-color: #9370DB;">
+      <span class="navbar-brand mb-0 h1" style="text-align: center;">Flowers Shop</span>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Header",
+  computed: {
+    ...mapGetters(['cart']),
+    totalQty() {
+      return this.cart.reduce((a, b) => a + b.qty, 0);
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  margin-bottom: 4rem;
+}
+</style>
